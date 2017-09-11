9888cfbbecd7125b336424c7b0e086976afc5ce3
Pietro Giuffrida <pietro.giuffri@gmail.com>
2017-09-11 23:34:20 +0200

    Revisione dei channels e dei messages in un'unica struttura

--- config.py
+++ config.py
@@ -4,84 +4,119 @@ logfilename = "reader.log"
 
 stati = {0: 'ON', 1: 'OFF'}
 
-channels = [
-  {"channel": "A", "pin": 37, "status": 1, 'name': 'pompa 1'},
-  {"channel": "B", "pin": 35, "status": 1, 'name': 'pompa 2'},
-  {"channel": "C", "pin": 33, "status": 1, 'name': 'pompa 3'},
-  {"channel": "D", "pin": 31, "status": 1, 'name': 'livello 1'},
-  {"channel": "E", "pin": 29, "status": 1, 'name': 'livello 2'},
-  {"channel": "F", "pin": 23, "status": 1, 'name': 'livello 3'},
-  {"channel": "G", "pin": 21, "status": 1, 'name': 'livello 4'},
-]
-
 delay = 2
 
-mongo_error_message = "IMPOSSIBILE CONNETTERSI A MONGODB"
-mongo_error_sbj = "IMPOSSIBILE CONNETTERSI A MONGODB"
+channels = {
 
-messages = {
-
-  37 : {
-    0: {'send': True,
-        'message': 'accensione 37',
-        },
-    1: {'send': True,
-        'message': 'spegnimento 37',
-        }
+  37: {
+    "channel": "A",
+    "status": 1,
+    'name': 'pompa 1',
+    'events': {
+      0: {
+        'send': True,
+        'message': 'POMPA 1 ON',
+      },
+      1: {
+        'send': True,
+        'message': 'pompa 1 off',
+      }
+    }
   },
 
-  35 : {
-    0: {'send': True,
-        'message': 'accensione 35',
-        },
-    1: {'send': True,
-        'message': 'spegnimento 35',
-        }
+  35: {
+    "channel": "B",
+    "status": 1,
+    'name': 'pompa 2',
+    'events': {
+      0: {
+        'send': True,
+        'message': 'POMPA 2 ON',
+      },
+      1: {
+        'send': True,
+        'message': 'pompa 2 off',
+      }
+    }
   },
 
-  33 : {
-    0: {'send': True,
-        'message': 'accensione 33',
-        },
-    1: {'send': True,
-        'message': 'spegnimento 33',
-        }
+  33: {
+    "channel": "C",
+    "status": 1,
+    'name': 'pompa 3',
+    'events': {
+      0: {
+        'send': True,
+        'message': 'POMPA 3 ON',
+      },
+      1: {
+        'send': True,
+        'message': 'pompa 3 off',
+      }
+    }
   },
 
-  31 : {
-    0: {'send': True,
-        'message': 'accensione 31',
-        },
-    1: {'send': True,
-        'message': 'spegnimento 31',
-        }
+  31: {
+    "channel": "D",
+    "status": 1,
+    'name': 'livello 1',
+    'events': {
+      0: {
+        'send': True,
+        'message': 'RAGGIUNTO LIVELLO FIUME 1',
+      },
+      1: {
+        'send': True,
+        'message': 'livello fiume 1 off',
+      }
+    }
   },
 
-  29 : {
-    0: {'send': True,
-        'message': 'accensione 29',
-        },
-    1: {'send': True,
-        'message': 'spegnimento 29',
-        }
+  29: {
+    "channel": "E",
+    "status": 1,
+    'name': 'livello 2',
+    'events': {
+      0: {
+        'send': True,
+        'message': 'RAGGIUNTO LIVELLO FIUME 2',
+      },
+      1: {
+        'send': True,
+        'message': 'livello fiume 2 off',
+      }
+    }
   },
 
-  23 : {
-    0: {'send': True,
-        'message': 'accensione 23',
-        },
-    1: {'send': True,
-        'message': 'spegnimento 23',
-        }
+  23: {
+    "channel": "F",
+    "status": 1,
+    'name': 'livello 3',
+    'events': {
+      0: {
+        'send': True,
+        'message': 'RAGGIUNTO LIVELLO FIUME 3',
+      },
+      1: {
+        'send': True,
+        'message': 'livello fiume 3 off',
+      }
+    }
   },
 
-  21 : {
-    0: {'send': True,
-        'message': 'accensione 21',
-        },
-    1: {'send': True,
-        'message': 'spegnimento 21',
+  21: {
+    "channel": "G",
+    "status": 1,
+    'name': 'livello 4',
+    'events': {
+      0: {
+        'send': True,
+        'message': 'RAGGIUNTO LIVELLO FIUME 4',
+      },
+      1: {
+        'send': True,
+        'message': 'livello fiume 4 off',
         }
+    }
   },
-
 }
