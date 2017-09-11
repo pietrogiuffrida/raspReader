fa852f4667ce7aff942e3bcecda008dee0a81d6b
Pietro Giuffrida <pietro.giuffri@gmail.com>
2017-09-11 22:08:48 +0200

    Definita struttura messaggi per ciascun canale/status

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+logfilename = "reader.log"
 
 channels = [
   {"name": "channel_a", "pin": 37, "status": 1},
@@ -10,3 +11,75 @@ channels = [
   {"name": "channel_f", "pin": 23, "status": 1},
   {"name": "channel_g", "pin": 21, "status": 1},
 ]
+
+delay = 2
+
+mongo_error_message = "IMPOSSIBILE CONNETTERSI A MONGODB"
+mongo_error_sbj = "IMPOSSIBILE CONNETTERSI A MONGODB"
+
+messages = {
+
+  37 : {
+    0: {'send': True,
+        'message': 'accensione 37',
+        },
+    1: {'send': True,
+        'message': 'spegnimento 37',
+        }
+  },
+
+  35 : {
+    0: {'send': True,
+        'message': 'accensione 35',
+        },
+    1: {'send': True,
+        'message': 'spegnimento 35',
+        }
+  },
+
+  33 : {
+    0: {'send': True,
+        'message': 'accensione 33',
+        },
+    1: {'send': True,
+        'message': 'spegnimento 33',
+        }
+  },
+
+  31 : {
+    0: {'send': True,
+        'message': 'accensione 31',
+        },
+    1: {'send': True,
+        'message': 'spegnimento 31',
+        }
+  },
+
+  29 : {
+    0: {'send': True,
+        'message': 'accensione 29',
+        },
+    1: {'send': True,
+        'message': 'spegnimento 29',
+        }
+  },
+
+  23 : {
+    0: {'send': True,
+        'message': 'accensione 23',
+        },
+    1: {'send': True,
+        'message': 'spegnimento 23',
+        }
+  },
+
+  21 : {
+    0: {'send': True,
+        'message': 'accensione 21',
+        },
+    1: {'send': True,
+        'message': 'spegnimento 21',
+        }
+  },
+
+}
