c7b2755564783b4090034a8f3f53f6deeb6e0501
Pietro Giuffrida <pietro.giuffri@gmail.com>
2017-09-11 03:18:30 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+channels = [
+  {"name": "channel_a", "pin": 37, "status": 1},
+  {"name": "channel_b", "pin": 35, "status": 1},
+  {"name": "channel_c", "pin": 33, "status": 1},
+  {"name": "channel_d", "pin": 31, "status": 1},
+  {"name": "channel_e", "pin": 29, "status": 1},
+  {"name": "channel_f", "pin": 23, "status": 1},
+  {"name": "channel_g", "pin": 21, "status": 1},
+]
